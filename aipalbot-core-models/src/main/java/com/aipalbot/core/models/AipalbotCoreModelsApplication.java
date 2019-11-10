39eb96ab97b7eef8ddd6ae0a3d5becea3fed064a
succesabbey <39390468+succesabbey@users.noreply.github.com>
2019-11-10 14:36:36 -0500

    Committing model default project

---
+++ aipalbot-core-models/src/main/java/com/aipalbot/core/models/AipalbotCoreModelsApplication.java
@@ -0,0 +1,13 @@
+package com.aipalbot.core.models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipalbotCoreModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipalbotCoreModelsApplication.class, args);
+	}
+
+}
---
+++ aipalbot-core-models/src/test/java/com/aipalbot/core/models/AipalbotCoreModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipalbot.core.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipalbotCoreModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
